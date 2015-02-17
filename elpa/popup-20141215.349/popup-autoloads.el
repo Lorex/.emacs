7051da37a50d8becd125a7c0ffbc864bd8c43d74
Lorex <joe50613@gmail.com>
2015-02-17 12:19:50 +0800

    Delete unused file

--- elpa/popup-20141215.349/popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21650 15111
-;;;;;;  957374 178000))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here
--- elpa/web-mode-20141217.712/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20141217.712" "major mode for editing html templates" (quote nil))
