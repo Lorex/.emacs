41b7ef2446b312dfded6c65165760cb733485d01
Lorex <joe50613@gmail.com>
2015-03-17 03:16:56 +0800

    revert to previus setting

---
+++ elpa/php-completion-0.3/php-completion-autoloads.el
@@ -0,0 +1,18 @@
+;;; php-completion-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("php-completion-pkg.el" "php-completion.el")
+;;;;;;  (21650 36825 568684 841000))
+
+;;;***
+
+(provide 'php-completion-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; php-completion-autoloads.el ends here
